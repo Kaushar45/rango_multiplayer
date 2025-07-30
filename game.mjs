225947cb333cf534c92ffc46a6dd223f8c2744af
Kaushar45 <45kaushar@gmail.com>
2025-07-30 06:56:34 +0530

    Refactor Game class to use consistent semicolon usage and improve code formatting

--- game.mjs
+++ game.mjs
@@ -1,20 +1,35 @@
 class Game {
-  #canvasSizeX
-  #canvasSizeY
-  #canvasElement
-  #canvas
+  #canvasSizeX;
+  #canvasSizeY;
+  #canvasElement;
+  #canvas;
 
   constructor(gameConfig) {
-    this.#canvasSizeX = gameConfig.canvasSizeX
-    this.#canvasSizeY = gameConfig.canvasSizeY
-    this.#canvasElement = document.getElementById(gameConfig.canvasId)
-    this.#canvasElement.height = this.#canvasSizeY
-    this.#canvasElement.width = this.#canvasSizeX
-    this.#canvas = this.#canvasElement.getContext("2d")
+    this.#canvasSizeX = gameConfig.canvasSizeX;
+    this.#canvasSizeY = gameConfig.canvasSizeY;
+    this.#canvasElement = document.getElementById(gameConfig.canvasId);
+    this.#canvasElement.height = this.#canvasSizeY;
+    this.#canvasElement.width = this.#canvasSizeX;
+    this.#canvas = this.#canvasElement.getContext("2d");
+    document.addEventListener("keypress", this.#keyInput);
   }
   run() {
-    console.log(this.#canvasSizeX, this.#canvasSizeY)
+    console.log(this.#canvasSizeX, this.#canvasSizeY);
+  }
+
+  #keyInput(event) {
+    if (event.code === "KeyA") {
+      console.log("left");
+    } else if (event.code === "KeyS") {
+      console.log("down");
+    } else if (event.code === "KeyW") {
+      console.log("up");
+    } else if (event.code === "KeyD") {
+      console.log("right");
+    } else if (event.code === "Space") {
+      console.log("fire");
+    }
   }
 }
 
-export default Game
+export default Game;
